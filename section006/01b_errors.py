d3a03c0130e5ca10cf09bf42bdad295fb3769bf4
Randy J Fortier <randy.fortier@uoit.ca>
2025-09-09 19:23:11 -0400

    Added conditionals for section 006

---
+++ section006/01b_errors.py
@@ -0,0 +1,25 @@
+# first_name = 'Randy" # syntax errors
+# x + 1 = 1
+
+first_name = 'Randy'
+print(first_name[0])
+# print(first_name[100]) # runtime error
+
+total = 77
+total += 65
+total += 92
+count = 2 # logic error
+print(f'{total = }')
+print(f'{count = }')
+print(f'average: {total / count = }')
+
+# calculate an average of a list
+
+# sum all of the numbers from the list
+# count the numbers in the list
+# divide the sum by the count
+
+# coding exercise 01b.1
+num1 = int(input('Enter the first number: '))
+num2 = int(input("Enter the second number: "))
+print(f'The answer is {(num1 + num2 ) % 5}')
---
+++ section006/02a_conditionals.py
@@ -0,0 +1,12 @@
+age = 17
+if age >= 19:
+    print('Have fun in the club!')
+    print('Be safe!')
+
+print('Goodbye!')
+
+final_mark = 54
+if final_mark < 50:
+    print('F')
+elif final_mark < 60:
+    print('D')
