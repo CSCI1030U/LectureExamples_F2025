8fa01a6d0bef6ef0f1bfbe7e0b39b2f98e787193
Randy J Fortier <randy.fortier@uoit.ca>
2025-09-22 19:26:39 -0400

    Added lists for 001

--- section001/03a_strings_lists.py
+++ section001/03a_strings_lists.py
@@ -1,2 +1,51 @@
 # 03a - Strings and Lists
 
+# coding exercise 03a.1
+full_name = 'Keanu Reaves'
+first_name = ''
+last_name = ''
+first = True 
+for letter in full_name:
+    if letter == ' ':
+        first = False 
+    elif first:
+        first_name += letter 
+    else:
+        last_name += letter
+print(f'{first_name = }')
+print(f'{last_name = }')
+
+full_name = 'Barack Obama'
+space_index = full_name.index(' ')
+first_name = full_name[:space_index]
+last_name = full_name[space_index + 1:]
+print(f'{first_name = }')
+print(f'{last_name = }')
+
+nums = [2,4,6,8,10,4,6,8]
+nums.remove(4)
+print(f'{nums = }')
+
+# print(f'{sum(["One", "Two", "Three"]) = }')
+
+matrix = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8]
+]
+print(f'{matrix[1][0] = }')
+
+# coding exercise 03a.2
+
+midterm_marks: list[float] = [41.0, 65.0, 72.5, 74.25, 55.0, 61.0, 84.5]
+
+# add up all the marks (find the sum)
+total = 0.0
+count = 0
+for mark in midterm_marks:
+    total += mark
+    count += 1
+
+# divide by the number of marks
+print(f'{total / count = }')
+print(f'{sum(midterm_marks) / len(midterm_marks) = }')
---
+++ section001/03b_dictionaries.py
@@ -0,0 +1,41 @@
+# coding exercise 03b.1
+
+sentence = 'ahmed runs quickly'
+words = []
+current_word = ''
+for letter in sentence:
+    if letter == ' ':
+        words.insert(0, current_word)
+        current_word = ''
+    else:
+        current_word += letter 
+words.insert(0, current_word)
+
+print(f'{words = }')
+
+reverse_sentence = ''
+for word in words:
+    reverse_sentence += word + ' '
+print(f'{reverse_sentence = }')
+
+words = 'ahmed runs quickly'.split(' ')
+reverse_words = words[::-1]
+reverse_sentence = ' '.join(reverse_words)
+print(f'{reverse_sentence = }')
+
+# dictionaries
+
+carla = {
+    'sid': 100000001,
+    'first name': 'Carla',
+    'last_name': 'Rodriguez',
+    'midterm_mark': 74.25
+}
+print(f'{carla["midterm_mark"] = }')
+
+# for key in carla:
+#     print(f'{key = }')
+
+for key in carla.keys():
+    print(f'{key = }, {carla[key] = }')
+print(f'{carla = }')
