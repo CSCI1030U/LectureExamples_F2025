3696d2b4ffffc13974a01d94f390b207f27c3646
Randy J Fortier <randy.fortier@uoit.ca>
2025-10-08 15:05:59 -0400

    Added OOP for section 006

--- section006/05a_recursion.py
+++ section006/05a_recursion.py
@@ -33,3 +33,34 @@ print(f'{fib(2) = }')
 print(f'{fib(5) = }')
 print(f'{fib(8) = }')
 # print(f'{fib(50) = }') # heat death of the universe
+
+# practice problem #3
+
+def is_reverse(str1: str, str2: str) -> bool:
+    # 1. base case
+    if len(str1) == 0 and len(str2) == 0:
+        return True
+
+    if len(str1) == 1 and len(str2) == 1:
+        # if str1[0] == str2[0]:
+        #     return True 
+        # else:
+        #     return False
+        return str1[0] == str2[0]
+
+    # 2. recursive case
+    # a. check if they one character that matches
+    # b. check if the rest of the two strings are reverse of one another
+    if str1[0] != str2[-1]:
+        return False
+    else:
+        # print(f'is_reverse({str1}, {str2})')
+        # print(f' {str1[1:] = }')
+        # print(f' {str2[0:-1] = }')
+        return is_reverse(str1[1:], str2[0:-1])
+
+print(f'{is_reverse("abcd", "dcba") = }')
+print(f'{is_reverse("abcd", "cdba") = }')
+print(f'{is_reverse("abcd", "abcd") = }')
+print(f'{is_reverse("a", "a") = }')
+print(f'{is_reverse("", "") = }')
--- section006/05b_oop1.py
+++ section006/05b_oop1.py
@@ -1,2 +1,21 @@
 # 05b - OOP 1
 
+class Undergraduate_Course:
+    def __init__(self):
+        # self.set_course_code('')
+        pass
+    
+    def __init__(self, course_code):
+        self.set_course_code(course_code)
+
+    def get_course_code(self):
+        return self.course_code
+    
+    def set_course_code(self, course_code):
+        self.course_code = course_code
+
+cs1030 = Undergraduate_Course()
+cs1030.set_course_code('CSCI1030U')
+
+cs1060 = Undergraduate_Course(course_code="CSCI1060U")
+
