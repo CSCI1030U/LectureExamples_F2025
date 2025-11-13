3a505e3115091d55193b880bac2d60d87dd4f464
Randy J Fortier <randy.fortier@uoit.ca>
2025-11-12 19:13:28 -0500

    Added unit testing for s001

--- section001/08b_exceptions_unit_testing.py
+++ section001/08b_exceptions_unit_testing.py
@@ -23,9 +23,50 @@ class LoginIncorrectError(Exception):
     pass 
 
 username = 'bsmith'
-password = 'password'
+password = 'openup'
 if username == 'bsmith' and password == 'openup':
     print('Access granted')
 else:
     raise LoginIncorrectError('Invalid login credentials')
 
+# programming exercise 08b.2
+class Student:
+    def __init__(self, gpa, name):
+        self.gpa = gpa
+        self.name = name
+        self.marks = []
+
+    def set_mark(self, course, mark):
+        self.marks.append(mark)
+
+    def get_average(self):
+        sum = 0
+        for mark in self.marks:
+            sum += mark
+        return sum / len(self.marks)
+
+import unittest 
+
+class Student_Test(unittest.TestCase):
+    def test_init(self):
+        clarissa = Student(0.0, 'Clarissa')
+        self.assertEqual(clarissa.gpa, 0.0)
+        # self.assertAlmostEqual(clarissa.gpa, 0.0, 0.00001)
+        self.assertEqual(clarissa.name, 'Clarissa')
+
+    def test_set_mark(self):
+        clarissa = Student(0.0, 'Clarissa')
+        self.assertEqual(clarissa.marks, [])
+        clarissa.set_mark('CSCI1030U', 71.5)
+        self.assertEqual(clarissa.marks, [71.5])
+        clarissa.set_mark('CSCI1060U', 68.25)
+        self.assertEqual(clarissa.marks, [71.5, 68.25])
+
+    def test_get_average(self):
+        clarissa = Student(0.0, 'Clarissa')
+        clarissa.set_mark('CSCI1030U', 71.0)
+        clarissa.set_mark('CSCI1060U', 68.0)
+        self.assertAlmostEqual(clarissa.get_average(), 69.5, 0.001)
+        
+
+unittest.main()
