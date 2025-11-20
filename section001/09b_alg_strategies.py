53ad8a178fa550b100206c07c9162b97493e4c15
Randy J Fortier <randy.fortier@uoit.ca>
2025-11-20 18:24:31 -0500

    alg sec006

--- section001/09b_alg_strategies.py
+++ section001/09b_alg_strategies.py
@@ -1,2 +1,60 @@
 # 09b - Algorithm Strategies
 
+def dc_search(elements, to_find):
+    if len(elements) == 0:
+        return False 
+    elif len(elements) == 1:
+        return elements[0] == to_find
+    
+    # print(f'{elements = }')
+
+    # divide
+    mid = len(elements) // 2
+    first = elements[:mid]
+    second = elements[mid:]
+
+    # conquer (recursively solve)
+    first_result = dc_search(first, to_find)
+    second_result = dc_search(second, to_find)
+
+    # combine
+    return first_result or second_result
+
+print(f'{dc_search([1,2,3,4,5,6,7,8], 5) = }')
+print(f'{dc_search([1,7,4,8,3,2,5,6], 5) = }')
+print(f'{dc_search([1,7,4,8,3,2,5,6], 15) = }')
+print(f'{dc_search([1,7,4,8,3,2,5,6], 1) = }')
+print(f'{dc_search([1,7,4,8,3,2,5,6], 6) = }')
+
+# coding exercise 09b.2
+def greedy_knapsack(available_items, max_weight):
+    pass 
+
+items = [
+    {
+        'name': 'Wooden Sculpture',
+        'weight': 2.0,
+        'value': 3.5
+    },
+    {
+        'name': 'Silver Earrings',
+        'weight': 1.0,
+        'value': 150.0
+    },
+    {
+        'name': 'Diamond Necklace',
+        'weight': 1.0,
+        'value': 750.0
+    },
+    {
+        'name': 'Stone Statue',
+        'weight': 45.0,
+        'value': 8.5
+    },
+    {
+        'name': 'Gold Bracelet',
+        'weight': 5.0,
+        'value': 275.0
+    },
+]
+print(f'{greedy_knapsack(items, 8.5) = }')
--- section006/09b_alg_strategies.py
+++ section006/09b_alg_strategies.py
@@ -1,2 +1,29 @@
 # 09b - Algorithm Strategies
 
+# coding exercise 09b.1
+
+def dc_search(elements, to_find):
+    # base case(s)
+    if len(elements) == 0:
+        return False 
+    elif len(elements) == 1:
+        return elements[0] == to_find
+
+    # divide
+    middle = len(elements) // 2
+    first = elements[:middle]
+    second = elements[middle:]
+
+    # conquer
+    first_result = dc_search(first, to_find)
+    second_result = dc_search(second, to_find)
+
+    # combine
+    return first_result or second_result
+
+print(f'{dc_search([4, 8, 1, -3, 9, 8, 3, -1, 10, 5], 3) = }')
+print(f'{dc_search([4, 8, 1, -3, 9, 8, 3, -1, 10, 5], -3) = }')
+print(f'{dc_search([4, 8, 1, -3, 9, 8, 3, -1, 10, 5], 10) = }')
+print(f'{dc_search([4, 8, 1, -3, 9, 8, 3, -1, 10, 5], 4) = }')
+print(f'{dc_search([4, 8, 1, -3, 9, 8, 3, -1, 10, 5], 5) = }')
+print(f'{dc_search([4, 8, 1, -3, 9, 8, 3, -1, 10, 5], 7) = }')
