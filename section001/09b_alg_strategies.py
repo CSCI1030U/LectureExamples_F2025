f60dd2c207b3e27c030a40f4fdab1d818aad688d
Randy J Fortier <randy.fortier@uoit.ca>
2025-11-26 12:11:47 -0500

    Added algorithm strategies

--- section001/09b_alg_strategies.py
+++ section001/09b_alg_strategies.py
@@ -27,8 +27,52 @@ print(f'{dc_search([1,7,4,8,3,2,5,6], 1) = }')
 print(f'{dc_search([1,7,4,8,3,2,5,6], 6) = }')
 
 # coding exercise 09b.2
+def insert_sort_by_ratio(array):
+    for j in range(1, len(array)):
+        key = array[j]
+        i = j - 1
+        while i >= 0 and array[i]['ratio'] < key['ratio']:
+            array[i + 1] = array[i]
+            i -= 1
+        array[i + 1] = key
+
+
 def greedy_knapsack(available_items, max_weight):
-    pass 
+    # calculate the value/weight for all items
+    value_to_weight = []
+    for item in available_items:
+        value_to_weight.append({
+            'name': item['name'],
+            'value': item['value'],
+            'weight': item['weight'],
+            'ratio': item['value'] / item['weight']
+        })
+    
+    # sort by value/weight in descending order
+    insert_sort_by_ratio(value_to_weight)
+    # print(f'{value_to_weight = }')
+
+    # choose the items from highest ratio until backpack is full
+    backpack = []
+    for item in value_to_weight:
+        if item['weight'] <= max_weight:
+            # take the whole item
+            backpack.append(item)
+            max_weight -= item['weight']
+        else:
+            # take the max fraction of this item
+            fraction = max_weight / item['weight']
+            partial_item = {
+                "name": item["name"],
+                'weight': item['weight'] * fraction,
+                'value': item['value'] * fraction,
+                'ratio': item['ratio']
+            }
+            backpack.append(partial_item)
+            # max_weight -= item['weight'] * fraction
+            return backpack
+
+
 
 items = [
     {
@@ -58,3 +102,14 @@ items = [
     },
 ]
 print(f'{greedy_knapsack(items, 8.5) = }')
+
+# coding exercise 09b.3
+def fib_dp(n: int) -> int:
+    solutions: list[int] = [0, 1]
+    if n < 2:
+        return solutions[n]
+    for i in range(2, n + 1):
+        solutions.append(solutions[i - 1] + solutions[i - 2])
+    return solutions[n]
+
+print(f'{fib_dp(100) = }')
--- section006/09b_alg_strategies.py
+++ section006/09b_alg_strategies.py
@@ -27,3 +27,62 @@ print(f'{dc_search([4, 8, 1, -3, 9, 8, 3, -1, 10, 5], 10) = }')
 print(f'{dc_search([4, 8, 1, -3, 9, 8, 3, -1, 10, 5], 4) = }')
 print(f'{dc_search([4, 8, 1, -3, 9, 8, 3, -1, 10, 5], 5) = }')
 print(f'{dc_search([4, 8, 1, -3, 9, 8, 3, -1, 10, 5], 7) = }')
+
+# coding exercise 09b.2
+
+items = [
+    {'name': 'Wooden Sculpture', 'value': 3.5, 'weight': 2.0},
+    {'name': 'Silver Earrings', 'value': 150.0, 'weight': 1.0},
+    {'name': 'Diamond Necklace', 'value': 750.0, 'weight': 1.0},
+    {'name': 'Stone Statue', 'value': 8.5, 'weight': 45.0},
+    {'name': 'Gold Bracelet', 'value': 275.0, 'weight': 5.0}
+]
+
+def insert_sort(elements):
+    for j in range(1, len(elements)):
+        key = elements[j]
+        i = j - 1
+        while i >= 0 and elements[i]['ratio'] < key['ratio']:
+            elements[i + 1] = elements[i]
+            i -= 1
+        elements[i + 1] = key
+
+def fractional_knapsack(available_items, capacity):
+    # calculate the value/weight ratio for each item
+    for item in available_items:
+        item['ratio'] = item['value'] / item['weight']
+
+    # sort the list of items in descending order by value/weight
+    insert_sort(available_items)
+
+    # insert items in order into the backpack until backpack is full
+    backpack = []
+    for item in available_items:
+        if item['weight'] <= capacity:
+            backpack.append(item)
+            capacity -= item['weight']
+        else:
+            fraction_to_take = capacity / item['weight']
+            part_item = {
+                'name': item['name'],
+                'value': item['value'] * fraction_to_take,
+                'weight': item['weight'] * fraction_to_take,
+                'ratio': item['ratio']
+            }
+            backpack.append(part_item)
+            return backpack
+
+
+print(f'{fractional_knapsack(items, 8.5) = }')
+
+# coding exercise 09b.3
+
+def fib_dp(n: int) -> int:
+    solutions : list[int] = [0, 1]
+    # if n < 2:
+    #     return n
+    for i in range(2, n + 1):
+        solutions.append(solutions[i - 1] + solutions[i - 2])
+    return solutions[n]
+
+print(f'{fib_dp(10000) = }')
