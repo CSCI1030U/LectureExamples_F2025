3e1b7279731f738b6540ca1ce1fd6e539da9748f
Randy J Fortier <randy.fortier@uoit.ca>
2025-11-27 08:17:01 -0500

    data structures - s001

--- section001/10a_basic_data_structures.py
+++ section001/10a_basic_data_structures.py
@@ -1,2 +1,25 @@
 # 10a - Basic Data Structures
 
+class Stack:
+    def __init__(self):
+        self.elements = []
+
+    def is_empty(self):
+        return len(self.elements) == 0
+
+    def push(self, new_element):
+        self.elements.append(new_element)
+
+    def pop(self):
+        return self.elements.pop()
+
+    def top(self):
+        return self.elements[-1]
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print(f'{stack.pop() = }')
+print(f'{stack.pop() = }')
+print(f'{stack.pop() = }')
