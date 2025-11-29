37d7483138a60439935a4024e3c6821d6fbdb99f
Randy J Fortier <randy.fortier@uoit.ca>
2025-11-29 08:05:49 -0500

    Added data structures for section 006

--- section006/10a_basic_data_structures.py
+++ section006/10a_basic_data_structures.py
@@ -1,2 +1,40 @@
 # 10a - Basic Data Structures
 
+class EmptyStackError(Exception):
+    pass
+
+class Stack:
+    def __init__(self):
+        self.elements = []
+
+    def push(self, new_element):
+        self.elements.append(new_element)
+    
+    def top(self):
+        if len(self.elements) == 0:
+            raise EmptyStackError('Cannot pop from an empty stack')
+
+        return self.elements[-1]
+    
+    def pop(self):
+        if len(self.elements) == 0:
+            raise EmptyStackError('Cannot pop from an empty stack')
+
+        return self.elements.pop()
+    
+    def is_empty(self):
+        return len(self.elements) == 0
+
+stack = Stack()
+print(f'{stack.is_empty() = }')
+stack.push('AAA')
+stack.push('BBB')
+stack.push('CCC')
+print(f'{stack.is_empty() = }')
+print(f'{stack.top() = }')
+print(f'{stack.pop() = }')
+stack.push('DDD')
+stack.push('EEE')
+print(f'{stack.pop() = }')
+print(f'{stack.pop() = }')
+print(f'{stack.pop() = }')
